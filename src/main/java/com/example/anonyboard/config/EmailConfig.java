8cb078466643895267004c73cc772c0983d1e7be
Baesiru <151522558+Baesiru@users.noreply.github.com>
2024-07-21 00:25:18 +0900

    토큰 테스트 성공으로 인한 AccessToken 기간 원상복귀, EmailConfig의 변수들 properties를 이용한 속성값 주입으로 변경

--- src/main/java/com/example/anonyboard/config/EmailConfig.java
+++ src/main/java/com/example/anonyboard/config/EmailConfig.java
@@ -1,5 +1,6 @@
 package com.example.anonyboard.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,23 +10,46 @@ import java.util.Properties;
 
 @Configuration
 public class EmailConfig {
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.port}")
+    private int port;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
+    @Value("${spring.mail.properties.protocol}")
+    private String protocol;
+    @Value("${spring.mail.properties.auth}")
+    private String auth;
+    @Value("${spring.mail.properties.socket}")
+    private String socket;
+    @Value("${spring.mail.properties.starttls}")
+    private String starttls;
+    @Value("${spring.mail.properties.debug}")
+    private String debug;
+    @Value("${spring.mail.properties.ssl.trust}")
+    private String trust;
+    @Value("${spring.mail.properties.protocols}")
+    private String sslProtocol;
+
     @Bean
     public JavaMailSender mailSender() {
 
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("pingol3397@gmail.com");
-        mailSender.setPassword("rqxu bwul dqvp fzww");
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties javaMailProperties = new Properties();
-        javaMailProperties.put("mail.transport.protocol", "smtp");
-        javaMailProperties.put("mail.smtp.auth", "true");
-        javaMailProperties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        javaMailProperties.put("mail.smtp.starttls.enable", "true");
-        javaMailProperties.put("mail.debug", "true");
-        javaMailProperties.put("mail.smtp.ssl.trust", "smtp.naver.com");
-        javaMailProperties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+        javaMailProperties.put("mail.transport.protocol", protocol);
+        javaMailProperties.put("mail.smtp.auth", auth);
+        javaMailProperties.put("mail.smtp.socketFactory.class", socket);
+        javaMailProperties.put("mail.smtp.starttls.enable", starttls);
+        javaMailProperties.put("mail.debug", debug);
+        javaMailProperties.put("mail.smtp.ssl.trust", trust);
+        javaMailProperties.put("mail.smtp.ssl.protocols", sslProtocol);
 
         mailSender.setJavaMailProperties(javaMailProperties);
 
